34334a1e2283995fbbe9121ef2b840931d365201
GitG3H <139292719+GitG3H@users.noreply.github.com>
2025-01-11 15:26:07 -0600

    Update app.py

--- Blog_Generation_in_AWS/app.py
+++ Blog_Generation_in_AWS/app.py
@@ -1,6 +1,7 @@
 import boto3
 import botocore.config
 import json
+import response
 
 from datetime import datetime
 
@@ -12,8 +13,8 @@ def blog_generate_using_bedrock(blogtopic:str)-> str:
     body={
         "prompt":prompt,
         "max_gen_len":512,
-        "temperature":0.5,
-        "top_p":0.9
+        "temperature":0.5, # randomness (0= predictable, 1= unpredictable)
+        "top_p":0.9 # aka nucleus sampling:  decides how many possible words to consider (high > more possible words >less likely ones > more diverse)
     }
 
     try:
@@ -24,24 +25,23 @@ def blog_generate_using_bedrock(blogtopic:str)-> str:
         response_content=response.get('body').read()
         response_data=json.loads(response_content)
         print(response_data)
-        blog_details=response_data['generation']
+        blog_details=response_data['generation'] #response from model is stored in key variable called "generation"
         return blog_details
     except Exception as e:
         print(f"Error generating the blog:{e}")
         return ""
 
+# Function to save blog details in S3 bucket.
 def save_blog_details_s3(s3_key,s3_bucket,generate_blog):
     s3=boto3.client('s3')
-
     try:
         s3.put_object(Bucket = s3_bucket, Key = s3_key, Body =generate_blog )
         print("Code saved to s3")
-
     except Exception as e:
         print("Error when saving the code to s3")
 
 
-
+# When API gateway sends any post event, it will be captured and hits particular trigger. We have to capture this event along with context and retrieve query.
 def lambda_handler(event, context):
     # TODO implement
     event=json.loads(event['body'])
@@ -49,13 +49,12 @@ def lambda_handler(event, context):
 
     generate_blog=blog_generate_using_bedrock(blogtopic=blogtopic)
 
+    # If blog is generated, store it in S3 bucket
     if generate_blog:
         current_time=datetime.now().strftime('%H%M%S')
         s3_key=f"blog-output/{current_time}.txt"
         s3_bucket='aws_bedrock_course1'
         save_blog_details_s3(s3_key,s3_bucket,generate_blog)
-
-
     else:
         print("No blog was generated")
 
